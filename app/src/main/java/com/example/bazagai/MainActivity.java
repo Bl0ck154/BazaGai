5daf37288472ea08106de706694202d417b44d67
Bl0ck <zail@dublin.com>
2019-09-14 20:40:38 +0300

    added ImageHelper, added enter key submit

--- app/src/main/java/com/example/bazagai/MainActivity.java
+++ app/src/main/java/com/example/bazagai/MainActivity.java
@@ -8,10 +8,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -21,25 +25,51 @@ import org.jsoup.select.Elements;
 
 import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity {
     public final String BAZA_URL = "https://baza-gai.com.ua";
-    TextView tv;
+    TextView tvTitle;
+    TextView tvStolen;
     EditText editNum;
     Editable number;
     ProgressBar progressBar;
     ImageView imageView;
+    ScrollView scrollView;
+    TextView tvRegister;
+    TextView tvFeatures;
+    TextView tvOperation;
+    TextView tvAddress;
+    Button btnGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = this.findViewById(R.id.tv);
+        tvTitle = this.findViewById(R.id.tvTitle);
         editNum = this.findViewById(R.id.editNum);
         progressBar = this.findViewById(R.id.progressBar);
         imageView = this.findViewById(R.id.img);
+        tvStolen = this.findViewById(R.id.tvStolenInfo);
+        scrollView = this.findViewById(R.id.scrollViewInfo);
+        tvRegister = this.findViewById(R.id.tvRegisterDate);
+        tvFeatures = this.findViewById(R.id.tvFeatures);
+        tvOperation = this.findViewById(R.id.tvOperation);
+        tvAddress = this.findViewById(R.id.tvAddress);
+        btnGo = this.findViewById(R.id.btnGo);
+
+        editNum.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    btnGo.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void btnClick(View view) {
@@ -49,15 +79,17 @@ public class MainActivity extends AppCompatActivity {
             progressBar.setVisibility(ProgressBar.VISIBLE);
         }
         catch (Exception ex) {
-
+            Log.e("ERROR", ex.getMessage());
         }
     }
 
     public class NewThreed extends AsyncTask<String, Void, String> {
 
         public Elements elements;
-        String text = "";
+        String title = "";
+        String stolen;
         Bitmap image;
+        ArrayList<String> tableColumns = new ArrayList<>();
 
         @Override
         protected String doInBackground(String... strings) {
@@ -65,40 +97,43 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Document doc = Jsoup.connect(BAZA_URL + "/nomer/" + number).get();
                 elements = doc.select(".text-center.mb-3");
-                for (Element element : elements) {
-                    text += element.text();
-                }
-                elements = doc.select(".card-img-top");
                 if(!elements.isEmpty()) {
-                    String imgUrl = BAZA_URL + elements.first().attr("src");
-                    image = this.imgDownload(imgUrl);
+                    title = elements.first().text();
+                    stolen = doc.select(".stolen-info").first().text();
+
+                    elements = doc.select("tbody").select("td");
+                    for(Element el : elements) {
+                        tableColumns.add(el.text());
+                    }
+
+                    elements = doc.select(".card-img-top");
+                    if (!elements.isEmpty()) {
+                        String imgUrl = BAZA_URL + elements.first().attr("src");
+                        image = ImageHelper.getRoundedCornerBitmap(ImageHelper.downloadByUrl(imgUrl), 16);
+                    }
                 }
             }
             catch (Exception ex) {
                 Log.e("Error", ex.getMessage());
-                text = ex.getMessage();
+                title = ex.getMessage();
             }
             return null;
         }
 
-        private Bitmap imgDownload(String url) {
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(url).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
         @Override
         protected void onPostExecute(String result) {
-
-            tv.setText(text);
             progressBar.setVisibility(ProgressBar.GONE);
+            if(title.isEmpty())
+                return;
+
+            tvTitle.setText(title);
             imageView.setImageBitmap(image);
+            tvStolen.setText(stolen);
+            tvRegister.setText(tableColumns.get(0));
+            tvFeatures.setText(tableColumns.get(2));
+            tvOperation.setText(tableColumns.get(3));
+            tvAddress.setText(tableColumns.get(4));
+            scrollView.setVisibility(View.VISIBLE);
         }
     }
 }
